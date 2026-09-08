5fd3825420db35b8580aff1f4fc733df8859462b
Anutosh Kumar Ray <anutoshkumarray@gmail.com>
2026-09-08 15:30:59 +0530

    Time: 1 ms (83.65%), Space: 42.5 MB (57.08%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -3,9 +3,6 @@ class Solution {
         if(n < 1000){
             return 0;
         }
-        if(n < 1000000){
-            return n - 999;
-        }
-        return 0;
+        return n-999;
     }
 }
